e92f220f4c609ffb61c5095ebaee63afbbe78c20
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2024-01-17 09:15:50 +0100

    Linkamos elementos html con js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const mensaje = document.getElementById("mensaje");
+const informacion = document.getElementById("informacion");
+const enviar = document.getElementById("enviar");
+const errores = document.getElementById("mensajesError");
